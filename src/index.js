c3181074c40299a7a151f2cc95ab9bdb094cfa2e
Larissa Rebeka <tec.melocavalcanti@gmail.com>
2024-08-30 00:06:47 -0300

    Função que vai chamar todas as outras, sendo o ponto de entrada que inicia todo o processo de corrida.

--- src/index.js
+++ src/index.js
@@ -17,3 +17,9 @@ const player2 = {
 async function rollDice() {
   return Math.floor(Math.random() * 6) + 1;
 }
+
+(async function main(){
+    console.log(
+        `🏁🚨 Corrida entre ${player1.NOME} e ${player2.NOME} começando... /n`
+    );
+});
