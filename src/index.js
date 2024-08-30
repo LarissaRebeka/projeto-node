a38478087b2057137c54603792dc9ee7091bb4a3
Larissa Rebeka <tec.melocavalcanti@gmail.com>
2024-08-30 00:22:24 -0300

    Race Engine (Functions Chains) + Rouns (For)

--- src/index.js
+++ src/index.js
@@ -18,6 +18,12 @@ async function rollDice() {
   return Math.floor(Math.random() * 6) + 1;
 }
 
+async function playRaceEngine(character1, character2) {
+    for (let round = 1; round <= 5; round++) {
+      console.log(`🏁 Rodada ${round}`);
+    }
+}
+
 (async function main(){
     console.log(
         `🏁🚨 Corrida entre ${player1.NOME} e ${player2.NOME} começando... /n`
