b68d4cdf421764346f5a1a3813b613325aeb15cb
Larissa Rebeka <tec.melocavalcanti@gmail.com>
2024-08-30 16:34:53 -0300

    Block (Switch Case)

--- src/index.js
+++ src/index.js
@@ -18,9 +18,31 @@ async function rollDice() {
   return Math.floor(Math.random() * 6) + 1;
 }
 
+async function getRandomBlock() {
+  let random = Math.random();
+  let result;
+
+  switch (true) {
+    case random < 0.33:
+      result = "RETA";
+      break;
+    case random < 0.66:
+      result = "CURVA";
+      break;
+    default:
+      result = "CONFRONTO";
+  }
+
+  return result;
+}
+
 async function playRaceEngine(character1, character2) {
     for (let round = 1; round <= 5; round++) {
       console.log(`🏁 Rodada ${round}`);
+
+      //sortear bloco
+      let block = await getRandomBlock()
+      console.log(`Bloco: ${block}`);
     }
 }
 
